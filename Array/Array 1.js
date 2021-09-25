6ab352d0f0ee4a5b97c7a73a662b640846724931
Diego Melo <diego-melo@uni9.edu.br>
2021-09-25 01:47:00 -0300

    Versão 1.2.0

---
+++ Array/Array 1.js
@@ -0,0 +1,32 @@
+//Array String
+var produtos = [
+    "Notebook Samsung X40",
+    "Smartphone Xiaomi Redmi 9",
+    "Smart TV LED 50 LG",
+    "Home Theater Philco PHT690"
+];
+
+var meses = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro"
+];
+
+//Array Números
+var notas_aluno = [ 9, 8, 7, 9 ];
+
+//Array Numeros  e String
+var funcionario = [
+    'Caio César Martins',   // Nome
+     23,                          // Idade
+    'caio@email.com'      // email
+];
