154aac747e655b07ed06d190acdea3373fb137d3
Diego Melo <diego-melo@uni9.edu.br>
2021-10-05 21:23:03 -0300

    Versão 1.10.0

---
+++ Objetos/ObjetoLiteral.js
@@ -0,0 +1,14 @@
+var aula = {
+    id: 10,
+    aluno: 'Diego',
+    tecnologia: 'JavaScript'
+};
+
+var usuario_logado = true;
+
+
+console.log(aula.id); // 10
+console.log(aula.aluno); // Diego
+console.log(aula.tecnologia); // JavaScript
+
+console.log(usuario_logado); // true
